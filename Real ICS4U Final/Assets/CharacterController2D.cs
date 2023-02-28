9d16eb31d2ed6cd1f019e87d85c9a5c9bb0342ca
Seungbeom Kim <kimse@students.dsbn.org>
2023-02-28 15:06:06 -0500

    PlayerAttack & Enemy
    Todo :
    flip sprite by moving direction
    Enemy die()
    Make hitbox into rectangle shape (currently in circular shape)

--- Real ICS4U Final/Assets/CharacterController2D.cs
+++ Real ICS4U Final/Assets/CharacterController2D.cs
@@ -6,10 +6,14 @@ public class CharacterController2D : MonoBehaviour
 {
     private Collision coll;
     public Rigidbody2D rb;
+    public LayerMask enemyLayer;
+    public Transform attackPoint;
 
     public float speed = 5f;
     public float jumpForce = 8f;
     public float slideSpeed = 3f;
+    public float attackRange = 5f;
+    public int attackDamage = 5;
     public bool canMove;
     public bool wallSlide;
 
@@ -43,6 +47,11 @@ public class CharacterController2D : MonoBehaviour
         {
             wallSlide = false;
         }
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Attack();
+        }
     }
 
     private void WallSide()
@@ -62,4 +71,28 @@ public class CharacterController2D : MonoBehaviour
         rb.velocity = new Vector2(rb.velocity.x, 0);
         rb.velocity += Vector2.up * jumpForce;
     }
+
+    private void Attack()
+    {
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayer);
+
+        foreach(Collider2D enemy in hitEnemies)
+        {
+            Debug.Log("hit");
+
+            enemy.GetComponent<Enemy>().TakeDamage(attackDamage);
+        }
+
+        
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if(attackPoint == null) return;
+
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+    }
+
+
+
 }
