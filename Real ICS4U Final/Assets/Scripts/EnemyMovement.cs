05ead8a3eaccde549baef5be6f49c20b21bb83bf
Seungbeom Kim <kimse@students.dsbn.org>
2023-03-24 14:51:05 -0400

    Enemy Agro, follow player

--- Real ICS4U Final/Assets/Scripts/EnemyMovement.cs
+++ Real ICS4U Final/Assets/Scripts/EnemyMovement.cs
@@ -5,12 +5,18 @@ using UnityEngine;
 public class EnemyMovement : MonoBehaviour
 {
     public Rigidbody2D rb;
+    public LayerMask playerMask;
     public float speed = 5f;
+    public float agroRadius = 10f;
+    public GameObject player;
+
+    private int side = 1;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        
     }
 
     // Update is called once per frame
@@ -21,11 +27,49 @@ public class EnemyMovement : MonoBehaviour
 
         Vector2 dir = new Vector2(x, y);
 
-        Walk(dir);
+        //Walk(dir);
+
+        if(InAgro())
+        {
+            //walk to player
+            if (player.transform.position.x > transform.position.x)
+            {
+                if (side == -1) Flip();
+                side = 1;
+                Walk(new Vector2(0.3f, 0));
+            }
+            else
+            {
+                if (side == 1) Flip();
+                side = -1;
+                Walk(new Vector2(-0.3f, 0));
+            }
+        }
     }
 
     private void Walk(Vector2 dir)
     {
         rb.velocity = (new Vector2(dir.x * speed, rb.velocity.y));
+    }
+
+    private bool InAgro()
+    {
+        if(Physics2D.OverlapCircle(transform.position, agroRadius, playerMask)) return true;
+        return false;
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(transform.position, agroRadius);
+    }
+
+    private void Flip()
+    {
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+        //Vector3 theScale2 = attackPoint.localScale;
+        //theScale2.x *= -1;
+        //attackPoint.localScale = theScale2;
     }
 }
