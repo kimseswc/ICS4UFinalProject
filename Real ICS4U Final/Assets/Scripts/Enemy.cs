a79da10b245c17d2664bf139661d84f903358f5d
Seungbeom Kim <kimse@students.dsbn.org>
2023-04-06 15:04:31 -0400

    Enemy Health Bar, Enemy Reward
    Enemies now give money when killed

--- Real ICS4U Final/Assets/Scripts/Enemy.cs
+++ Real ICS4U Final/Assets/Scripts/Enemy.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Enemy : MonoBehaviour
 {
+    public Transform player;
     public Rigidbody2D self;
-    public int maxHealth = 100;
-    int currentHealth;
+    public int maxHealth = 150;
+    public int health;
+    public int reward = 50;
+    public Transform EnemyHealthBar;
 
     // Start is called before the first frame update
     void Start()
     {
         self = GetComponent<Rigidbody2D>();
-        currentHealth = maxHealth;
+        health = maxHealth;
+
     }
 
     // Update is called once per frame
@@ -23,14 +28,14 @@ public class Enemy : MonoBehaviour
 
     public void TakeDamage(int damage)
     {
-        currentHealth -= damage;
-        Debug.Log(currentHealth);
-        if(currentHealth <= 0) Die();
+        health -= damage;
+        if(health <= 0) Die();
+        EnemyHealthBar.GetComponent<Image>().fillAmount = (float)health / (float)maxHealth;
     }
 
     void Die()
     {
+        player.GetComponent<CharacterController2D>().money += reward;
         Destroy(gameObject);
-        Debug.Log("die");
     }
 }
