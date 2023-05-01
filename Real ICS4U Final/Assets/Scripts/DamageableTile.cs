cde19c2e868dbf6e7a9199f77e4489a3e2332310
Seungbeom Kim <kimse@students.dsbn.org>
2023-05-01 14:38:21 -0400

    Tile that does damage when player steps on it
    eg lava, spike

---
+++ Real ICS4U Final/Assets/Scripts/DamageableTile.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.Tilemaps;
+
+public class DamageableTile : MonoBehaviour
+{
+
+    public int damage = 10;
+    public float tick = 10f;
+    private bool canDamagePlayer = true;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(canDamagePlayer && collision.tag == "Player")
+        {
+            StartCoroutine(damageWait());
+            collision.GetComponent<CharacterController2D>().TakeDamage(damage);
+        }
+    }
+
+    IEnumerator damageWait()
+    {
+        canDamagePlayer = false;
+        yield return new WaitForSeconds(tick);
+        canDamagePlayer = true;
+    }
+}
